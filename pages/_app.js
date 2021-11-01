d63ec1e936d28c3d94d440a8e4259f521e5f6864
GeoBrodas <geobro2310@gmail.com>
2021-11-01 08:35:32 +0530

    add og

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,33 @@ import { Toaster } from 'react-hot-toast';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Meta tags og */}
+        <meta property="og:title" content="Adaalat" />
+        <meta
+          property="og:description"
+          content="A court management system to add, register case, withdraw case seamlessly."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dapafwlvo/image/upload/v1635735765/image1_kafi3l.png"
+        />
+        <meta property="og:url" content="https://adaalat.vercel.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        {/* Twitter */}
+        <meta name="twitter:title" content="Adaalat" />
+        <meta
+          name="twitter:description"
+          content="A court management system to add, register case, withdraw case seamlessly."
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dapafwlvo/image/upload/v1635735765/image1_kafi3l.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <Toaster />
       <Layout>
         <Component {...pageProps} />
