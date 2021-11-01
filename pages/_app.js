645f26c7294c6ae198d80790366c215b6d505125
GeoBrodas <geobro2310@gmail.com>
2021-11-01 11:11:48 +0530

    add route progress onChange

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,16 @@ import { Provider } from 'next-auth/client';
 import Layout from '@/components/Layout/Layout';
 
 import Head from 'next/head';
+
 import { Toaster } from 'react-hot-toast';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         {/* Meta tags og */}
         <meta property="og:title" content="Adaalat" />
         <meta
@@ -35,6 +38,11 @@ function MyApp({ Component, pageProps }) {
         />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+      <NextNProgress
+        color="#700B97"
+        height={3}
+        options={{ showSpinner: false }}
+      />
       <Toaster />
       <Layout>
         <Component {...pageProps} />
