adaef86088f559eff9f7ec77baed315a93a4111a
GeoBrodas <geobro2310@gmail.com>
2021-11-02 22:38:20 +0530

    fix og-tags

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,31 @@ export default function Home() {
           name="description"
           content="Adaalat: One step Solution to managing court hearings"
         />
+
+        {/* Meta tags og */}
+        <meta property="og:title" content="Adaalat" />
+        <meta
+          property="og:description"
+          content="A court management system to add, register case, withdraw case seamlessly."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dapafwlvo/image/upload/v1635735765/image1_kafi3l.png"
+        />
+        <meta property="og:url" content="https://adaalat.vercel.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        {/* Twitter */}
+        <meta name="twitter:title" content="Adaalat" />
+        <meta
+          name="twitter:description"
+          content="A court management system to add, register case, withdraw case seamlessly."
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dapafwlvo/image/upload/v1635735765/image1_kafi3l.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <HomePage />
