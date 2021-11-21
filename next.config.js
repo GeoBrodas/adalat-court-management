85207f2e89f6b27899bfd2accdfbb6ebfdc78432
GeoBrodas <geobro2310@gmail.com>
2021-11-21 18:32:45 +0530

    update next-config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true,
     runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
     disable: process.env.NODE_ENV === 'development',
   },
   images: {
